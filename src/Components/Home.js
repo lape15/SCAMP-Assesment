4cc3b08ea391b901495d4a0d84203dd3d94fb759
Lape15 <larape1993@gmail.com>
2020-04-28 00:46:23 +0100

    countries can be searched for

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,21 +2,40 @@ import React, { useState } from 'react'
 import axios from 'axios'
 const Home = () => {
   const [stat, setStat] = useState([])
+  const [searchField, setSearchField] = useState('')
   axios
     .get('https://api.covid19api.com/summary')
     .then((response) => {
-      // console.log(response.data)
       setStat(response.data.Countries)
     })
     .catch((error) => {
       console.log(error)
     })
-  // console.log(stat)
+  const handeleSearch = (e) => {
+    setSearchField(e.target.value)
+    console.log(searchField)
+  }
+
+  const filteredCountries = stat.filter((country) => {
+    return (
+      country.Country.toLowerCase().indexOf(searchField.toLowerCase()) !== -1
+    )
+  })
   return (
     <div className="w-auto  p-6 h-auto bg-transparent">
       <h2 className="text-center text-blue-800 italic lg:text-3xl mb-6 md: text-xl text-center">
         Get your daily COVID19 updates here
       </h2>
+      <div class="w-full max-w-xs">
+        <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <input
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            placeholder="Country"
+            onChange={handeleSearch}
+          />
+        </form>
+      </div>
       <div className="w-full bg-gray-400 text-gray-400 border border-solid rounded border-black h-full">
         <div className="flex  lg:w-auto lg:bg-gray-300 ">
           <div className="lg:flex-1 m-12  w-8 text-blue-800 font-black tracking-wider">
@@ -39,7 +58,7 @@ const Home = () => {
           </div>
         </div>
 
-        {stat.map((country, index) => (
+        {filteredCountries.map((country, index) => (
           <div
             className="flex w-auto  border
             border-solid
